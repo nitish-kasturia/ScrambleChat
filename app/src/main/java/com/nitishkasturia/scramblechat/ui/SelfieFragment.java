04a54851ade5078ba9caf641f2e7bef9d8686635
HiImShoaib <nerdyshaikh@gmail.com>
2016-01-23 18:36:56 -0500

    added SurfaceView, but it's still faulty rn...

--- app/src/main/java/com/nitishkasturia/scramblechat/ui/SelfieFragment.java
+++ app/src/main/java/com/nitishkasturia/scramblechat/ui/SelfieFragment.java
@@ -1,14 +1,17 @@
 package com.nitishkasturia.scramblechat.ui;
 
-import android.content.Intent;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.nitishkasturia.scramblechat.R;
+
+import java.io.IOException;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,8 +19,6 @@ import com.nitishkasturia.scramblechat.R;
 
 
 public class SelfieFragment extends Fragment {
-
-    Button mCameraButton;
 
     public SelfieFragment() {
         // Required empty public constructor
@@ -30,19 +31,50 @@ public class SelfieFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the layout for this fragment.
+
         View view = inflater.inflate(R.layout.fragment_selfie, container, false);
+        SurfaceView surfaceView = (SurfaceView) view.findViewById(R.id.surfaceView);
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
 
-        mCameraButton = (Button) view.findViewById(R.id.cameraButton);
-
-        mCameraButton.setOnClickListener(new View.OnClickListener() {
+        surfaceHolder.addCallback(new SurfaceHolder.Callback(){
+            private Camera mCamera;
             @Override
-            public void onClick(View v) {
-                Intent cameraOpen = new Intent("android.media.action.IMAGE_CAPTURE");
-                startActivity(cameraOpen);
+            public void surfaceCreated(SurfaceHolder holder) {
+                mCamera = android.hardware.Camera.open();
+
+                try {
+                    mCamera.setPreviewDisplay(holder);
+                }catch(IOException exception) {
+                    mCamera.release();
+                    mCamera = null;
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                mCamera.startPreview();
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                mCamera.stopPreview();
+                mCamera.release();
+                mCamera = null;
             }
         });
 
+
+//        mCameraButton = (Button) view.findViewById(R.id.cameraButton);
+//
+//        mCameraButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent cameraOpen = new Intent("android.media.action.IMAGE_CAPTURE");
+//                startActivity(cameraOpen);
+//            }
+//        });
+//
         return view;
     }
 }
