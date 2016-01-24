861625630251871f3cb13d8b3dd7dca065d0eee8
Nitish Kasturia <nitishkasturia@gmail.com>
2016-01-24 01:19:22 -0500

    merge

--- app/src/main/java/com/nitishkasturia/scramblechat/ui/LoginActivity.java
+++ app/src/main/java/com/nitishkasturia/scramblechat/ui/LoginActivity.java
@@ -8,8 +8,27 @@ import com.nitishkasturia.scramblechat.R;
 public class LoginActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+    public void init(Bundle savedInstanceState) {
+
+        // Add your slide's fragments here.
+        // AppIntro will automatically generate the dots indicator and buttons.
+        addSlide(LandingFragment.newInstance());
+        addSlide(SelfieFragment.newInstance());
+        addSlide(ScrambleFragment.newInstance());
+        addSlide(ShareFragment.newInstance());
+
+        // OPTIONAL METHODS
+        // Override bar/separator color.
+        // setBarColor(Color.parseColor("#3F51B5"));
+        // setSeparatorColor(Color.parseColor("#2196F3"));
+
+        // Hide Skip/Done button.
+        showSkipButton(false);
+        setProgressButtonEnabled(false);
+
+        // Turn vibration on and set intensity.
+        // NOTE: you will probably need to ask VIBRATE permission in Manifest.
+        setVibrate(true);
+        setVibrateIntensity(30);
     }
 }
