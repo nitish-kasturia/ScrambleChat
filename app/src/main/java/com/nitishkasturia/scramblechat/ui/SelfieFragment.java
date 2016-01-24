6764ac1f46fd639c8b06519b36a7207f659359ad
Nitish Kasturia <nitishkasturia@gmail.com>
2016-01-24 01:58:17 -0500

    Camera improvements

--- app/src/main/java/com/nitishkasturia/scramblechat/ui/SelfieFragment.java
+++ app/src/main/java/com/nitishkasturia/scramblechat/ui/SelfieFragment.java
@@ -1,7 +1,9 @@
 package com.nitishkasturia.scramblechat.ui;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,13 +12,16 @@ import android.widget.Button;
 
 import com.nitishkasturia.scramblechat.R;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 
-
 public class SelfieFragment extends Fragment {
 
+    String mCurrentPhotoPath;
     Button mCameraButton;
 
     public SelfieFragment() {
@@ -38,11 +43,41 @@ public class SelfieFragment extends Fragment {
         mCameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent cameraOpen = new Intent("android.media.action.IMAGE_CAPTURE");
-                startActivity(cameraOpen);
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                // Ensure that there's a camera activity to handle the intent
+                if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    // Create the File where the photo should go
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                                Uri.fromFile(photoFile));
+                        startActivityForResult(takePictureIntent, 1);
+                    }
+                }
             }
         });
 
         return view;
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String imageFileName = "LOGIN_PICTURE";
+        File storageDir = getActivity().getFilesDir();
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
     }
 }
